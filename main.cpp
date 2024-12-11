72d929158db169ba77fbf9a0884c441d7f49b877
UraAkonka <lebzyak321@mail.ru>
2024-12-11 18:59:15 +0500

    Функция для чтения оглавления архива RAR4

--- main.cpp
+++ main.cpp
@@ -1,73 +1,91 @@
 #include <iostream>
 #include <fstream>
-#include <windows.h>
 #include <vector>
+
 #include <cstdint>
 
 using namespace std;
 
+// Установить 1-байтовое выравнивание
 #pragma pack(push, 1)
 
-struct header {
-    uint16_t header_crc;
-    uint8_t header_type;
-    uint16_t header_flags;
-    uint16_t header_size;
-    char padding[8]; // Пропуск 8 байтов
+// Структура заголовка архива RAR4
+struct RarHeader
+{
+    uint16_t crc;          // 2-байтовое целое без знака
+    uint8_t type;          // 1-байтовое целое без знака
+    uint16_t flags;
+    uint16_t size;
 };
 
-
-
-struct filehead {
-    uint8_t PackSize;
-    uint8_t UnpSize;
-    uint8_t HostOS;
-    uint8_t FileCRC;
-    uint8_t FileTime;
-    uint8_t UnpVer;
-    uint8_t Method;
-    uint8_t NameSize;
-    uint8_t FileAttr;
-    uint16_t FileName;
-    uint32_t PackedData;
+// Структура заголовка упакованного файла
+struct FileHead
+{
+    uint32_t packSize;     // Размер упакованных данных
+    uint32_t unpackSize;   // Размер распакованных данных
+    uint16_t fileNameSize; // Длина имени файла
+    uint16_t headerSize;   // Размер заголовка записи
 };
 
+// Восстановить выравнивание
 #pragma pack(pop)
 
-void rar(){
-    ifstream rar_file("C:\\1111\\Example.rar");
-    if (!rar_file.is_open()){
-        cout << "Неудалось открыть файл" << endl;
+// Функция для чтения оглавления архива RAR4
+void readRarContents(const vector<char>& rarData)
+{
+    size_t offset = 7; // Начало первой записи после 7 байтов заголовка
+
+    while (offset < rarData.size())
+    {
+        RarHeader* header = const_cast<RarHeader*>(reinterpret_cast<const RarHeader*>(&rarData[offset]));
+        offset += sizeof(RarHeader);
+
+        if (header->type == 0x74) // Тип записи 0x74 - упакованный файл
+        {
+            FileHead* fileHead = const_cast<FileHead*>(reinterpret_cast<const FileHead*>(&rarData[offset]));
+            offset += sizeof(FileHead);
+
+            // Имя файла начинается после заголовка FILE_HEAD
+            char* fileName = const_cast<char*>(&rarData[offset + sizeof(FileHead)]);
+            offset += fileHead->packSize;
+
+            cout << "Имя файла: " << fileName << endl;
+            cout << "Размер упакованных данных: " << fileHead->packSize << endl;
+            cout << "Размер распакованных данных: " << fileHead->unpackSize << endl;
+        }
+        else
+        {
+            offset += header->size; // Пропуск неинтересных записей
+        }
     }
-    else{
-        rar_file.seekg(0,ios::end);
-        int size = rar_file.tellg();
-        cout << size << endl;
-
-        vector<char> rar_data(size, 0);
-
-        rar_file.read(rar_data.data(), size);
-
-        header *p_header = reinterpret_cast<header*> (&rar_data[7]);
-
-        cout << int(p_header->header_size) << endl;
-        cout << int(p_header->header_type) << endl;
-
-
-        rar_file.seekg(0,ios::beg);
-        char c;
-        rar_file.get(c);
-        cout << int(c);
-
-    }
-
 }
-
-
 
 int main()
 {
-    setlocale(LC_ALL,"Russian");
-    rar();
+    setlocale(LC_ALL, "RUS");
+    // Открыть файл в двоичном режиме
+    ifstream rarFile("D:\\rar\\Example.rar", ios::binary);
+
+    // Убедиться, что открылся
+    if (rarFile.is_open())
+    {
+        // Определить размер файла
+        rarFile.seekg(0, ios::end);
+        int fileSize = rarFile.tellg();
+        cout << "Размер файла: " << fileSize << " байт" << endl;
+
+        // Снова спозиционироваться в начало файла
+        rarFile.seekg(0, ios::beg);
+
+        // Выделить память
+        vector<char> rarData(fileSize, 0);
+
+        // Считать данные
+        rarFile.read(rarData.data(), fileSize);
+
+        // Чтение оглавления архива
+        readRarContents(rarData);
+    }
+
     return 0;
 }
